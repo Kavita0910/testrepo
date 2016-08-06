7bf3cd08722b52aaa9c607c01865b4ac3e6e427d
Kavita0910 <kavita.gurav9@gmail.com>
2016-08-06 17:35:10 +0530

    Update DemoA.java

--- src/qsp/DemoA.java
+++ src/qsp/DemoA.java
@@ -4,7 +4,8 @@ public class DemoA {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test"); \\comment
+		System.out.println("test"); 
+		\\comment
 	}
 
 }
